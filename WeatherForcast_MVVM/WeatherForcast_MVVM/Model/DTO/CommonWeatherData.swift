66f38f870997a45cfd71d6bd3e8c5c102728176f
bamsak <qjatjrdl963@naver.com>
2024-05-07 22:11:00 +0900

    [Refactor] CurrentWeather와 WeeklyWeather에 공용으로 사용될 모델 네임스페이스로 관리
    - CommonWeatherData

---
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Model/DTO/CommonWeatherData.swift
@@ -0,0 +1,93 @@
+//
+//  CommonWeatherData.swift
+//  WeatherForcast_MVVM
+//
+//  Created by BOMBSGIE on 5/6/24.
+//
+
+/// DTO NameSpace
+enum DTO { 
+    /// Model for both current and weekly weather
+    enum CommonWeatherData { }
+}
+
+extension DTO.CommonWeatherData {
+    struct Coordinate: Decodable {
+        let longitude: Double
+        let latitude: Double
+        
+        private enum CodingKeys: String, CodingKey {
+            case longitude = "lon"
+            case latitude = "lat"
+        }
+    }
+}
+
+extension DTO.CommonWeatherData {
+    struct Main: Decodable {
+        let temperature: Double
+        let feelsLikeTemperature: Double
+        let minimumTemperature: Double
+        let maximumTemperature: Double
+        let pressure: Int
+        let humidity: Int
+        let seaLevel: Int
+        let groundLevel: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case temperature = "temp"
+            case feelsLikeTemperature = "feels_like"
+            case minimumTemperature = "temp_min"
+            case maximumTemperature = "temp_max"
+            case pressure, humidity
+            case seaLevel = "sea_level"
+            case groundLevel = "grnd_level"
+        }
+    }
+}
+
+extension DTO.CommonWeatherData {
+    struct Weather: Decodable {
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+}
+
+extension DTO.CommonWeatherData {
+    struct Wind: Decodable {
+        let speed: Double
+        let direction: Int
+        let gust: Double
+        
+        private enum CodingKeys: String, CodingKey {
+            case speed, gust
+            case direction = "deg"
+        }
+    }
+}
+
+extension DTO.CommonWeatherData {
+    struct Rain: Decodable {
+        let oneHour: Double?
+        let threeHour: Double?
+        
+        private enum CodingKeys: String, CodingKey {
+            case oneHour = "1h"
+            case threeHour = "3h"
+        }
+    }
+}
+
+extension DTO.CommonWeatherData {
+    struct Snow: Decodable {
+        let oneHour: Double?
+        let threeHour: Double?
+        
+        private enum CodingKeys: String, CodingKey {
+            case oneHour = "1h"
+            case threeHour = "3h"
+        }
+    }
+}
--- WeatherForcast_MVVM/WeatherForcast_MVVM/Model/DTO/CurrentWeather.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Model/DTO/CurrentWeather.swift
@@ -7,14 +7,14 @@
 
 extension DTO {
     struct CurrentWeather: Decodable {
-        let coordinate: Coordinate
-        let weather: [Weather]
+        let coordinate: CommonWeatherData.Coordinate
+        let weather: [CommonWeatherData.Weather]
         let base: String
-        let main: Main
+        let main: CommonWeatherData.Main
         let visibility: Int
-        let wind: Wind
-        let rain: Rain?
-        let snow: Snow?
+        let wind: CommonWeatherData.Wind
+        let rain: CommonWeatherData.Rain?
+        let snow: CommonWeatherData.Snow?
         let dataTime: Int
         let system: System
         let timezone: Int
@@ -30,88 +30,7 @@ extension DTO {
     }
 }
 
-extension DTO {
-    struct Coordinate: Decodable {
-        let longitude: Double
-        let latitude: Double
-        
-        private enum CodingKeys: String, CodingKey {
-            case longitude = "lon"
-            case latitude = "lat"
-        }
-    }
-}
-
-extension DTO {
-    struct Weather: Decodable {
-        let id: Int
-        let main: String
-        let description: String
-        let icon: String
-    }
-}
-
-extension DTO {
-    struct Main: Decodable {
-        let temperature: Double
-        let feelsLikeTemperature: Double
-        let minimumTemperature: Double
-        let maximumTemperature: Double
-        let pressure: Int
-        let humidity: Int
-        let seaLevel: Int
-        let groundLevel: Int
-        
-        private enum CodingKeys: String, CodingKey {
-            case temperature = "temp"
-            case feelsLikeTemperature = "feels_like"
-            case minimumTemperature = "temp_min"
-            case maximumTemperature = "temp_max"
-            case pressure, humidity
-            case seaLevel = "sea_level"
-            case groundLevel = "grnd_level"
-        }
-    }
-}
-
-extension DTO {
-    struct Wind: Decodable {
-        let speed: Double
-        let direction: Int
-        let gust: Double
-        
-        private enum CodingKeys: String, CodingKey {
-            case speed, gust
-            case direction = "deg"
-        }
-    }
-}
-
-extension DTO {
-    struct Rain: Decodable {
-        let oneHour: Double?
-        let threeHour: Double?
-        
-        private enum CodingKeys: String, CodingKey {
-            case oneHour = "1h"
-            case threeHour = "3h"
-        }
-    }
-}
-
-extension DTO {
-    struct Snow: Decodable {
-        let oneHour: Double?
-        let threeHour: Double?
-        
-        private enum CodingKeys: String, CodingKey {
-            case oneHour = "1h"
-            case threeHour = "3h"
-        }
-    }
-}
-
-extension DTO {
+extension DTO.CurrentWeather {
     struct System: Decodable {
         let type: Int
         let id: Int
