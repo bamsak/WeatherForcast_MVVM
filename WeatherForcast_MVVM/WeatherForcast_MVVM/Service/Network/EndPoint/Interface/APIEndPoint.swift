ed8491138986e083fcaeb3edba57fd19cd7b5160
bamsak <qjatjrdl963@naver.com>
2024-05-10 21:45:59 +0900

    [Feat] APIEndPoint 프로토콜 정의
    - OpenWeatherAPI의 URLRequest를 만들기 위한 프로토콜 추상화

---
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Network/EndPoint/Interface/APIEndPoint.swift
@@ -0,0 +1,15 @@
+//
+//  APIEndPoint.swift
+//  WeatherForcast_MVVM
+//
+//  Created by BOMBSGIE on 5/10/24.
+//
+
+import Foundation
+
+protocol APIEndPoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var queries: [URLQueryItem]? { get throws }
+}
