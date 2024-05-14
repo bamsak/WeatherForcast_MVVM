ac71720dc1734d43568d743fbd2c3f2d3a04dc81
bamsak <qjatjrdl963@naver.com>
2024-05-14 17:35:13 +0900

    [Refactor] NetworkService의 URLSession에 대한 의존성 역전
    - URLSessionProtocol을 정의하고 URLSession Extension으로 해당 프로토콜을 채택

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Network/NetworkService.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Network/NetworkService.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 final class NetworkService {
-    private let session: URLSession
+    private let session: URLSessionProtocol
     
-    init(session: URLSession = URLSession.shared) {
+    init(session: URLSessionProtocol = URLSession.shared) {
         self.session = session
     }
     
     func fetchData(_ urlRequest: URLRequest) async throws -> Data {
-        let (data, response) = try await session.data(for: urlRequest)
+        let (data, response) = try await session.data(for: urlRequest, delegate: nil)
         guard let statusCode = (response as? HTTPURLResponse)?.statusCode
         else {
             throw NetworkError.failedResponseCasting
