46a2c5ba377ff0e9be2501929a1750e4514e3079
bamsak <qjatjrdl963@naver.com>
2024-05-24 23:12:06 +0900

    [Feat] 위도, 경도 외부로 반환기능 구현
    - CheckedContinuation을 통한 Swift Concurrency를 활용하여 외부로 위도, 경도를 반환해줄 수 있도록 구현.

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Location/LocationService.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Location/LocationService.swift
@@ -9,11 +9,18 @@ import CoreLocation
 
 final class LocationService: NSObject {
     private let manager: CLLocationManager = CLLocationManager()
+    private var continueation: CheckedContinuation<CLLocationCoordinate2D, Error>?
     
     override init() {
         super.init()
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func fetchCoordinate() async throws -> CLLocationCoordinate2D {
+        return try await withCheckedThrowingContinuation { continueation in
+            self.continueation = continueation
+        }
     }
 }
 
@@ -27,6 +34,15 @@ extension LocationService: CLLocationManagerDelegate {
         default:
             break
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last
+        else {
+            return
+        }
+        continueation?.resume(returning: location.coordinate)
+        continueation = nil
     }
 }
 
