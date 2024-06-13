82e2517296605cc5fe640ed65dc9ac12f9ccbde2
bamsak <qjatjrdl963@naver.com>
2024-06-13 19:23:14 +0900

    [Feat] NSCache 관리하는 객체 CacheService 구현

---
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Service/Cache/Implementation/CacheService.swift
@@ -0,0 +1,32 @@
+//
+//  CacheService.swift
+//  WeatherForcast_MVVM
+//
+//  Created by BOMBSGIE on 6/13/24.
+//
+
+import Foundation
+
+final class CacheService {
+    private let storage = NSCache<NSString, NSData>()
+    static let shared = CacheService()
+    
+    private init() { }
+}
+
+extension CacheService: Cacheable {
+    func fetchCachedData(with key: String) -> Data? {
+        let cacheKey = NSString(string: key)
+        guard let cachedData = storage.object(forKey: cacheKey)
+        else {
+            return nil
+        }
+        return Data(cachedData)
+    }
+    
+    func storeCacheData(_ data: Data, for key: String) {
+        let cacheKey = NSString(string: key)
+        let cacheData = NSData(data: data)
+        storage.setObject(cacheData, forKey: cacheKey)
+    }
+}
