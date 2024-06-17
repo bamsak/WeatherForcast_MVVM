d569c70abae7946d2a464b0435a215cda1c8cd66
bamsak <qjatjrdl963@naver.com>
2024-06-17 21:40:38 +0900

    [Feat] 날씨 관련 UseCase의 공통된 날씨 Entity 구현

---
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Model/Entity/UseCase/CommonWeatherDetail.swift
@@ -0,0 +1,27 @@
+//
+//  CommonWeatherDetail.swift
+//  WeatherForcast_MVVM
+//
+//  Created by BOMBSGIE on 6/17/24.
+//
+
+import Foundation
+
+extension Entity.UseCase {
+    enum CommomWeatherDetail {
+        struct Weather {
+            let main: String
+            let description: String
+            let iconData: Data
+        }
+    }
+}
+
+extension Entity.UseCase.CommomWeatherDetail {
+    struct TemperatureDetail {
+        let temperature: Double
+        let feelsLikeTemperature: Double
+        let minimumTemperature: Double
+        let maximumTemperature: Double
+    }
+}
