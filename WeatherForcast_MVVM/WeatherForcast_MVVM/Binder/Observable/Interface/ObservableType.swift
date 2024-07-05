78d9d2519b0a1c48384f19ca2ef77ccedb98d438
bamsak <qjatjrdl963@naver.com>
2024-07-05 19:24:22 +0900

    [Feat] ObservableType extension으로 subscribe메소드 구현.
    - extension으로 구현된 메소드 내부에서 Observer를 생성하고 실 구현체의 subscribe메소드를 호출하여 Disposable 객체 반환 하도록 구현.

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Binder/Observable/Interface/ObservableType.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Binder/Observable/Interface/ObservableType.swift
@@ -11,3 +11,26 @@ protocol ObservableType: AnyObject {
     func subscribe(_ observer: Observer<Element>) -> Disposable
     func asObservable() -> Observable<Element>
 }
+
+extension ObservableType {
+    func subscribe(_ handler: @escaping (Event<Element>) -> Void) -> Disposable {
+        let observer = Observer<Element> { event in
+            handler(event)
+        }
+        return self.asObservable().subscribe(observer)
+    }
+    
+    func subscribe(onNext: ((Element) -> Void)? = nil, onError: ((Error) -> Void)? = nil) -> Disposable {
+        let observer = Observer<Element> { event in
+            switch event {
+            case .onNext(let element):
+                onNext?(element)
+            case .onError(let error):
+                if let onError = onError {
+                    onError(error)
+                }
+            }
+        }
+        return self.asObservable().subscribe(observer)
+    }
+}
