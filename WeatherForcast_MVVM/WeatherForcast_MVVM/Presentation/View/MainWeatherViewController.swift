76d65519984cbb095cd9c8922901eed4c33956b5
bamsak <qjatjrdl963@naver.com>
2024-07-08 23:30:09 +0900

    [Feat] 의존성을 주입을 위한 AppEnvironment 객체 구현.

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/MainWeatherViewController.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/MainWeatherViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 final class MainWeatherViewController: UIViewController {
-
+    private let mainWeatherViewModel: MainWeatherViewModel
+    
+    init(mainWeatherViewModel: MainWeatherViewModel) {
+        self.mainWeatherViewModel = mainWeatherViewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
