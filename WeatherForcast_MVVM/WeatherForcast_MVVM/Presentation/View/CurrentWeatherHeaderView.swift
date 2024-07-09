3a67e197513868e561a7009137d508103dfb2ee2
bamsak <qjatjrdl963@naver.com>
2024-07-09 22:36:08 +0900

    [Feat] HeaderView의 요소 AutoLayout 구현 및 update메소드 구현

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/CurrentWeatherHeaderView.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/CurrentWeatherHeaderView.swift
@@ -49,4 +49,69 @@ final class CurrentWeatherHeaderView: UICollectionReusableView {
         stackView.spacing = 8
         return stackView
     }()
+    
+    // MARK: - Initializer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHeaderView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+
+    func updateUI(with currentWeather: CurrentWeather) {
+        weatherIconView.image = UIImage(data: currentWeather.weather.iconData)
+        locationLabel.text = locationLabelText(currentWeather.location)
+        minMaxTemperatureLabel.text = minMaxTemperatureLabelText(currentWeather.temperaturDetail)
+        currentTemperatureLabel.text = currentWeather.temperaturDetail.temperature
+    }
+}
+
+// MARK: - TypeAlias
+
+extension CurrentWeatherHeaderView {
+    typealias CurrentWeather = Presentation.AllWeather.CurrentWeatherModel
+    typealias TemperatureDetail = Presentation.AllWeather.CommonWeather.TemperatureDetail
+}
+
+// MARK: - Private Method
+
+private extension CurrentWeatherHeaderView {
+    func locationLabelText(_ location: CurrentWeather.Location) -> String? {
+        guard let city = location.city
+        else {
+            return nil
+        }
+        guard let district = location.district
+        else {
+            return city
+        }
+        return city + district
+    }
+    
+    func minMaxTemperatureLabelText(_ temperature: TemperatureDetail) -> String {
+        return "최저 \(temperature.minimumTemperature) 최고 \(temperature.maximumTemperature)"
+    }
+}
+
+
+// MARK: - AutoLayout
+
+private extension CurrentWeatherHeaderView {
+    func configureHeaderView() {
+        addSubview(mainHeaderStackView)
+        
+        NSLayoutConstraint.activate([
+            mainHeaderStackView.topAnchor.constraint(equalTo: topAnchor),
+            mainHeaderStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainHeaderStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainHeaderStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            weatherIconView.widthAnchor.constraint(equalTo: heightAnchor, multiplier: 1.0)
+        ])
+    }
 }
