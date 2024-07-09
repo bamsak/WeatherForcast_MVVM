08cb9055d3f4feac327ab647487bb32c1c196ec4
bamsak <qjatjrdl963@naver.com>
2024-07-09 23:08:12 +0900

    [Feat] CompositionalLayout을 활용한 CollectionView 구현

--- WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/MainWeatherViewController.swift
+++ WeatherForcast_MVVM/WeatherForcast_MVVM/Presentation/View/MainWeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainWeatherViewController.swift
 //  WeatherForcast_MVVM
 //
 //  Created by BOMBSGIE on 4/29/24.
@@ -8,8 +8,22 @@
 import UIKit
 
 final class MainWeatherViewController: UIViewController {
+    
+    // MARK: - stored Property
+
     private let mainWeatherViewModel: MainWeatherViewModel
     
+    // MARK: - UIComponents
+    
+    private lazy var weatherCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureCollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+
+    // MARK: - Initializer
+
     init(mainWeatherViewModel: MainWeatherViewModel) {
         self.mainWeatherViewModel = mainWeatherViewModel
         super.init(nibName: nil, bundle: nil)
@@ -19,9 +33,32 @@ final class MainWeatherViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - View LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
 }
 
+// MARK: - Configure CollectionView
+
+private extension MainWeatherViewController {
+    func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return .init { sectionIndex, layoutEnvironment in
+            var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+            configuration.headerMode = .supplementary
+            configuration.backgroundColor = .clear
+            
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                    heightDimension: .fractionalHeight(0.15))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, 
+                                                                     elementKind: UICollectionView.elementKindSectionHeader,
+                                                                     alignment: .top)
+            let section = NSCollectionLayoutSection.list(using: configuration,
+                                                         layoutEnvironment: layoutEnvironment)
+            section.boundarySupplementaryItems = [header]
+            return section
+        }
+    }
+}
